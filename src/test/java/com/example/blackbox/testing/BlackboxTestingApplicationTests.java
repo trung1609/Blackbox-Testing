a58ace58b2ed83b5afd30613fe191aeb1c3a8e84
trung1609 <vmt162005@gmail.com>
2026-04-20 07:27:54 +0700

    first commit

---
+++ src/test/java/com/example/blackbox/testing/BlackboxTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackbox.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackboxTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
